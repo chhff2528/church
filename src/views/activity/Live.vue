54762e4e6ac11e79adba04b02ed624f100cf599f
happyff <1391739579@qq.com>
2020-08-01 18:39:34 +0800

    Create Live.vue

---
+++ src/views/activity/Live.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="index">
+    <div id="player"></div>
+  </div>
+</template>
+<script>
+export default {
+  data: function() {
+    return {};
+  },
+  mounted: function() {
+   var playerLive = polyvObject("#player").livePlayer({
+     
+      width: "800px",
+      height: "533px",
+      flashvars: {
+        is_barrage: "on",
+        VolumeM: "0"
+      },
+      uid: "e405i666d0",
+      vid: "234028"
+    });
+  }
+};
+</script>
+<style scoped>
+[v-cloak] {
+  display: none;
+}
+
+.index {
+  margin-top: 1.2rem;
+}
+</style>
