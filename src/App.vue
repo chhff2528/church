5513e2de5f7a0245927e4aea65391504aa63377a
happyff <1391739579@qq.com>
2020-08-05 12:55:19 +0800

    Update App.vue
    更新app.vue

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,14 @@ export default {
   },
   components: {
     Header
+  },
+   mounted() {
+    window.addEventListener('hashchange', (e) => {
+      var currentPath = window.location.hash.slice(1); // 获取输入的路由
+      if (this.$router.path !== currentPath) {
+        this.$router.push(currentPath); // 动态跳转
+      }
+    }, false);
   }
 };
 </script>
